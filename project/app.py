78557356f80d1030e4e137ec438b8c0858998ed1
artes26 <ayush16r@gmail.com>
2025-09-20 02:17:07 +0530

    q

--- project/app.py
+++ project/app.py
@@ -83,5 +83,6 @@ def get_booking():
 
 # ---------------- Run ----------------
 if __name__ == "__main__":
+       app.run(debug=True)
     port = int(os.environ.get("PORT", 5000))  # Render provides PORT env
     app.run(host="0.0.0.0", port=port, debug=False)
